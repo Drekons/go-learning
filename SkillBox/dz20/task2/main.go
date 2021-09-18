3204b884e1b0125135c912557af67da63ff2346f
Alexander Drek <drek2005@gmail.com>
2021-09-18 15:26:31 +0300

    SkillBox DZ 20 task2

--- SkillBox/dz20/task2/main.go
+++ SkillBox/dz20/task2/main.go
@@ -2,16 +2,53 @@ package main
 
 import "fmt"
 
-func parseTest(sentences []string, chars []rune) {
+const SentenceCount = 4
+const charsCount = 5
 
+func parseTest(sentences [SentenceCount]string, chars [charsCount]rune) (result [SentenceCount][charsCount]int) {
+	for i, sentence := range sentences {
+		lastWordChars := getLastWordLikeArray(sentence)
+		for j, char := range chars {
+			result[i][j] = findRuneInArray(char, lastWordChars)
+		}
+	}
+
+	return
+}
+
+func getLastWordLikeArray(sentence string) (lastWordArr []rune) {
+	for i := 0; i < len(sentence); i++ {
+		if sentence[i] == ' ' {
+			lastWordArr = []rune{}
+			continue
+		}
+
+		lastWordArr = append(lastWordArr, rune(sentence[i]))
+	}
+
+	return
+}
+
+func findRuneInArray(char rune, wordChars []rune) (index int) {
+	index--
+
+	for k, wordChar := range wordChars {
+		if wordChar == char {
+			index = k
+			break
+		}
+	}
+
+	return
 }
 
 func main() {
 	fmt.Println("*** Задание 2. Поиск символов в нескольких строках ***")
 
-	sentences := [4]string{"Hello world", "Hello Skillbox", "Привет Мир", "Привет Skillbox"}
-	chars := [5]rune{'H', 'E', 'L', 'П', 'М'}
+	sentences := [SentenceCount]string{"Hello world", "Hello Skillbox", "Привет Мир", "Привет Skillbox"}
+	chars := [charsCount]rune{'o', 'е', 'i', 'd', 'l'}
 
 	fmt.Printf("Массив предложений %v\n", sentences)
 	fmt.Printf("Массив символов %v\n", chars)
+	fmt.Printf("Вхождения символов в последнее слово каждого предложения %v\n", parseTest(sentences, chars))
 }
